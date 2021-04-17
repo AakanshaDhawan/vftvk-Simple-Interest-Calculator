2f93b1136894fb5571ea5c32cd653bcd47c66361
Aakansha Dhawan <aakansha2809@gmail.com>
2021-04-17 13:58:34 +0000

    corrected the Simple Interest Calculator as per the needs

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function computeInterest()
      var principal = document.getElementById("principal").value;
 
      //Check if principal  is 0 or negative
-    if(principal.value < 1){
+    if(principal < 1){
         alert("Please enter a positive value");
         document.getElementById("principal").focus();
         return false;
@@ -12,7 +12,7 @@ function computeInterest()
      var years = document.getElementById("years").value;
      //Calculate Interest
      var interest = principal * years * rate /100;
-     var year = new Date().getFullYear()+parseInt(years) + "%";
+     var year = new Date().getFullYear()+parseInt(years) ;
      var amount = principal + interest
      //show the result
      document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
